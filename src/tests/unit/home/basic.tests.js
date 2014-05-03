a91bab3837364905986e8c3637b3847ca8555c12
Keats <balthek@gmail.com>
2014-05-03 22:58:34 +0100

    Trying a karma failure

--- src/tests/unit/home/basic.tests.js
+++ src/tests/unit/home/basic.tests.js
@@ -3,7 +3,7 @@ describe('Unit: Home module', function() {
   beforeEach(module('ngBoilerplate.home'));
 
   it('should have a dummy test', function() {
-    expect(true).to.be.true;
+    expect(true).to.be.false;
   });
 
 });
