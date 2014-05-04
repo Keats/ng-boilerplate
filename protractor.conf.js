6d1e9d4fe47d99993b94dd7da91610cf3664194d
Keats <balthek@gmail.com>
2014-05-04 11:02:19 +0100

    I want some fails!

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,15 +1,15 @@
 exports.config = {
-  // The file path to the selenium server jar.
-  seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar',
-  chromeOnly: false,
-  capabilities: {
-    browserName: 'firefox'
-  },
-  specs: ['src/tests/integration/**/*.js'],
-  baseUrl: 'http://localhost:8001',
-  framework: 'mocha',
-  mochaOpts: {
-    ui: 'bdd',
-    reporter: 'list'
-  }
+    // The file path to the selenium server jar.
+    seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar',
+    chromeOnly: false,
+    capabilities: {
+        browserName: 'firefox'
+    },
+    specs: ['src/tests/integration/**/*.js'],
+    baseUrl: 'http://localhost:8001',
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        reporter: 'list'
+    }
 };
