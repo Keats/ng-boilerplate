b17de15ea7e20d3b682333dc2a66d9703486d8d9
Keats <balthek@gmail.com>
2014-06-16 21:16:49 +0100

    Fix selenium version

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     // The file path to the selenium server jar.
-    seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar',
+    seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.42.0.jar',
     chromeOnly: false,
     capabilities: {
         browserName: 'firefox'
