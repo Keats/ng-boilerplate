1136a6c3d9ed4c4be10b30d6c172c7aee891951e
Keats <balthek@gmail.com>
2014-10-14 19:13:51 +0100

    Update 3rd party ts defs

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,7 +2,6 @@
 /// <reference path="libs/jquery.d.ts"/>
 /// <reference path="libs/angular.d.ts"/>
 /// <reference path="libs/angular-ui-router.d.ts"/>
-/// <reference path="libs/lodash.d.ts"/>
 
 
 // API TYPES
